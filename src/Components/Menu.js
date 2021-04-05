2825938eb12c02ca7397e4367b235b2d4366ff74
Fabien <pic.fab@hotmail.fr>
2021-04-05 15:53:25 +0200

    error urls

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import { kasa } from '../utils/picto'
 import {
     Link,
     withRouter,
-} from "react-router-dom";
+} from "react-router-dom"
 
 class MenuComponent extends Component {
     render() {
-        const { match} = this.props
+        const { match } = this.props
         return (
             <nav className="navPrimary">
                 {kasa}
                 <ul className="navPrimary__list">
-                    <li className="navPrimary__elt"><Link className={`navPrimary__link${match.path==='/'?' active':''}`} to="/">Accueil</Link></li>
+                    <li className="navPrimary__elt"><Link className={`navPrimary__link${match.path === '/' ? ' active' : ''}`} to="/">Accueil</Link></li>
                     <li className="navPrimary__elt"><Link className={`navPrimary__link${match.path === '/a-propos' ? ' active' : ''}`} to="/a-propos">À Propos</Link></li>
                 </ul>
             </nav>
