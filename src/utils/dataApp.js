f52f9d8e0468b95ea39f2b5953cd192a31d1d5f1
Fabien <pic.fab@hotmail.fr>
2021-04-05 11:25:29 +0200

    set siteUrl on PORT 3000

--- src/utils/dataApp.js
+++ src/utils/dataApp.js
@@ -1,7 +1,7 @@
 /**
  * @const {string} siteUrl Url of the app
  */
-export const siteUrl = 'http://localhost:3000/'
+export const siteUrl = 'http://localhost:3000'
 export const requestOptions = {
   method: 'GET',
   redirect: 'follow',
