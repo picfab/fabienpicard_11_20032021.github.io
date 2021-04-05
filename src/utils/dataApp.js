14377303bca3ab532880a47a2f93a04ea82be1a6
Fabien <pic.fab@hotmail.fr>
2021-04-05 16:14:59 +0200

    test

--- src/utils/dataApp.js
+++ src/utils/dataApp.js
@@ -1,7 +1,7 @@
 /**
  * @const {string} siteUrl Url of the app
  */
-export const siteUrl = 'http://picfab.github.io'
+export const siteUrl = 'https://picfab.github.io/fabienpicard_11_20032021.github.io'
 export const requestOptions = {
   method: 'GET',
   redirect: 'follow',
