02d5ae832871505486278c0940c9945a3276dd83
Fabien <pic.fab@hotmail.fr>
2021-04-05 16:37:10 +0200

    test basename

--- src/Routes.js
+++ src/Routes.js
@@ -10,11 +10,11 @@ export default class Routes extends Component {
         const { appart, apropos, home, error } = this.props.urls
         return (
             <Router>
-                <Switch>
-                    <Route path="/fabienpicard_11_20032021.github.io/:id" children={appart} />
-                    <Route path="/fabienpicard_11_20032021.github.io/a-propos" children={apropos} />
-                    <Route exact path="/fabienpicard_11_20032021.github.io/" children={home} />
-                    <Route path="/fabienpicard_11_20032021.github.io/404" children={error} />
+                <Switch baseName='/fabienpicard_11_20032021.github.io'>
+                    <Route path="/appartements/:id" children={appart} />
+                    <Route exact path="/a-propos" children={apropos} />
+                    <Route exact path="/" children={home} />
+                    <Route path="/404" children={error} />
                     <Route children={error} />
                 </Switch>
             </Router>
